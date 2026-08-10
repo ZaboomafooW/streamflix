d63d12156ca2d07116807cb7d62ce4ae6130236d
ZaboomafooW <kernjrw@gmail.com>
2026-08-10 06:33:13 -0500

    fix(tv): retain home data across rail navigation

--- app/src/main/java/com/streamflixreborn/streamflix/fragments/home/HomeTvFragment.kt
+++ app/src/main/java/com/streamflixreborn/streamflix/fragments/home/HomeTvFragment.kt
@@ -22,15 +22,12 @@ import com.streamflixreborn.streamflix.models.Category
 import com.streamflixreborn.streamflix.models.Episode
 import com.streamflixreborn.streamflix.models.Movie
 import com.streamflixreborn.streamflix.models.TvShow
-import com.streamflixreborn.streamflix.utils.viewModelsFactory
 import kotlinx.coroutines.Runnable
 import com.streamflixreborn.streamflix.utils.CacheUtils
 import kotlinx.coroutines.launch
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.streamflixreborn.streamflix.utils.LoggingUtils
-import com.streamflixreborn.streamflix.utils.UserPreferences
-import com.streamflixreborn.streamflix.utils.ProviderChangeNotifier
 
 class HomeTvFragment : Fragment() {
 
@@ -40,14 +37,13 @@ class HomeTvFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel: HomeViewModel by lazy {
-        val providerKey = UserPreferences.currentProvider?.name ?: "default"
         val factory = object : ViewModelProvider.Factory {
             override fun <T : ViewModel> create(modelClass: Class<T>): T {
                 @Suppress("UNCHECKED_CAST")
                 return HomeViewModel(AppDatabase.getInstance(requireContext())) as T
             }
         }
-        ViewModelProvider(this, factory).get(providerKey, HomeViewModel::class.java)
+        ViewModelProvider(requireActivity(), factory)[HomeViewModel::class.java]
     }
 
     private val appAdapter = AppAdapter()
@@ -68,16 +64,6 @@ class HomeTvFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initializeHome()
-
-        // Lightweight refresh when provider changes
-        viewLifecycleOwner.lifecycleScope.launch {
-            ProviderChangeNotifier.providerChangeFlow.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED).collect {
-                viewModel.getHome()
-            }
-        }
-
-        // Initial load
-        viewModel.getHome()
 
         viewLifecycleOwner.lifecycleScope.launch {
             viewModel.state.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED).collect { state ->
