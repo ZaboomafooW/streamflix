480b32bb30fa6fc1eff8c6a15306c72c088ebc8b
ZaboomafooW <kernjrw@gmail.com>
2026-08-10 03:18:40 -0500

    feat: carry movie original language metadata

--- app/src/main/java/com/streamflixreborn/streamflix/models/Movie.kt
+++ app/src/main/java/com/streamflixreborn/streamflix/models/Movie.kt
@@ -38,6 +38,9 @@ class Movie(
     @Ignore
     val recommendations: List<Show> = listOf(),
     override var isFavorite: Boolean = false,
+
+    @Ignore
+    var originalLanguage: String? = null,
 ) : Show, WatchItem, AppAdapter.Item {
 
     var released = released?.toCalendar()
@@ -90,6 +93,7 @@ class Movie(
         cast: List<People> = this.cast,
         recommendations: List<Show> = this.recommendations,
         isFavorite: Boolean = this.isFavorite,
+        originalLanguage: String? = this.originalLanguage,
     ) = Movie(
         id,
         title,
@@ -108,6 +112,7 @@ class Movie(
         cast,
         recommendations,
         isFavorite,
+        originalLanguage,
     )
 
     override fun equals(other: Any?): Boolean {
