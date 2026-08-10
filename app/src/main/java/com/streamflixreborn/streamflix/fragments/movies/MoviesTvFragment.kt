b1e4e4241da3d264a93a4c0e377aa00b4d80749b
ZaboomafooW <kernjrw@gmail.com>
2026-08-10 06:30:57 -0500

    fix(tv): retain movie catalog state across rail navigation

--- app/src/main/java/com/streamflixreborn/streamflix/fragments/movies/MoviesTvFragment.kt
+++ app/src/main/java/com/streamflixreborn/streamflix/fragments/movies/MoviesTvFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.flowWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
@@ -18,7 +20,6 @@ import com.streamflixreborn.streamflix.models.Movie
 import com.streamflixreborn.streamflix.providers.Provider
 import com.streamflixreborn.streamflix.utils.UserPreferences
 import com.streamflixreborn.streamflix.utils.CacheUtils
-import com.streamflixreborn.streamflix.utils.viewModelsFactory
 import kotlinx.coroutines.launch
 
 class MoviesTvFragment : Fragment() {
@@ -29,7 +30,15 @@ class MoviesTvFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val database by lazy { AppDatabase.getInstance(requireContext()) }
-    private val viewModel by viewModelsFactory { MoviesViewModel(database) }
+    private val viewModel: MoviesViewModel by lazy {
+        val factory = object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                @Suppress("UNCHECKED_CAST")
+                return MoviesViewModel(database) as T
+            }
+        }
+        ViewModelProvider(requireActivity(), factory)[MoviesViewModel::class.java]
+    }
 
     private val appAdapter = AppAdapter()
 
