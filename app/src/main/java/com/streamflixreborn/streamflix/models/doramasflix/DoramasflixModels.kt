2529c6fdcb010992a48375133d46a771cd30c2d8
ZaboomafooW <kernjrw@gmail.com>
2026-08-10 02:23:14 -0500

    Add Doramasflix rating response field

--- app/src/main/java/com/streamflixreborn/streamflix/models/doramasflix/DoramasflixModels.kt
+++ app/src/main/java/com/streamflixreborn/streamflix/models/doramasflix/DoramasflixModels.kt
@@ -30,6 +30,7 @@ data class Show(
     val nameEs: String? = null,
     val slug: String,
     val overview: String? = null,
+    val rating: Double? = null,
     @SerializedName("poster_path")
     val posterPath: String? = null,
     val poster: String? = null,
