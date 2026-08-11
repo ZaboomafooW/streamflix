6c8cea1187f8f7edbd1588334368c35628d6f040
ZaboomafooW <kernjrw@gmail.com>
2026-08-11 13:49:37 -0500

    fix(ridomovies): gate artwork network traffic

---
+++ app/src/main/java/com/streamflixreborn/streamflix/StreamFlixGlideModule.kt
@@ -0,0 +1,29 @@
+package com.streamflixreborn.streamflix
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Registry
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
+import com.bumptech.glide.load.model.GlideUrl
+import com.bumptech.glide.module.AppGlideModule
+import com.streamflixreborn.streamflix.utils.RidomoviesArtworkRateLimitInterceptor
+import okhttp3.OkHttpClient
+import java.io.InputStream
+
+@GlideModule
+class StreamFlixGlideModule : AppGlideModule() {
+
+    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
+        val client = OkHttpClient.Builder()
+            .addInterceptor(RidomoviesArtworkRateLimitInterceptor())
+            .build()
+        registry.replace(
+            GlideUrl::class.java,
+            InputStream::class.java,
+            OkHttpUrlLoader.Factory(client),
+        )
+    }
+
+    override fun isManifestParsingEnabled() = false
+}
